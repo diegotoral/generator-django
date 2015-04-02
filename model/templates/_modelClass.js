e63e9c5ea07a606271cc7352c23dbca96be2ed2e
Diego Toral <diegotoral@gmail.com>
2015-04-01 22:29:28 -0300

    Fixes 2 spaces indentantion

--- model/templates/_modelClass.js
+++ model/templates/_modelClass.js
@@ -1,6 +1,8 @@
 
 class <%= className %>(django.db.models.Model):
-<% _.each(fields, function(field) { %>  <%= field.asString %>
+<% _.each(fields, function(field) { %>
+    <%= field.asString %>
 <% }) %>
-  def __unicode__(self):
-    <% if(unicode !== 'pass'){ %>return <% } %><%= unicode %>
+
+    def __unicode__(self):
+        <% if(unicode !== 'pass'){ %>return <% } %><%= unicode %>
--- model/templates/_modelTest.js
+++ model/templates/_modelTest.js
@@ -1,9 +1,9 @@
 class <%= className %>TestCase(django.test.TestCase):
-  def setUp(self):
-    pass
+    def setUp(self):
+        pass
 
-  def tearDown(self):
-    pass
+    def tearDown(self):
+        pass
 
-  def test_unicode(self):
-    pass
+    def test_unicode(self):
+        pass
